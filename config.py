96137397a6d00b67e0cf61fd834f9c2443ac7eb2
eaytkn <emreaytkn@gmail.com>
2024-03-18 15:06:55 +0300

    Test dataloader for rgb images.

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ test_modality = 'rgb_frames'
 
 step_size = 1  # Skip number of frames in a clip
 
+test_batch_size = 16
 test_num_frames = 16
 
 test_img_resize = (224)
--- test.py
+++ test.py
@@ -1,13 +1,17 @@
+import torchvision.utils
 from torchvision import transforms
+from torch.utils.data import DataLoader
 import torchvision.transforms.functional as F
 import meccano
 
+import matplotlib.pyplot as plt
 import config as cfg
 
 if __name__ == '__main__':
     data_dir = cfg.data_dir
     modality = cfg.test_modality
 
+    batch_size = cfg.test_batch_size
     num_frames = cfg.test_num_frames
 
     step_size = cfg.step_size
@@ -20,3 +24,18 @@ if __name__ == '__main__':
 
     # Create dataset
     test_dataset = meccano.Meccano(data_dir, 'test', modality, step_size, num_frames, test_transform)
+
+    # Create dataloader
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=0)
+
+    images, labels = next(iter(test_dataloader)) # Extract one batch of images and labels
+    grid_img = torchvision.utils.make_grid(images, nrow=8)
+    grid_img = grid_img.numpy().transpose((1, 2, 0))
+    plt.figure(figsize=(15, 15))
+    plt.imshow(grid_img)
+    plt.axis('off')
+    plt.show()
+
+
+
+
